3c888c458f4026114ebe9c6821669d9d81071e6f
saiddlkc <saidalkilic@gmail.com>
2024-05-22 15:53:17 +0200

    connected with mongodb server

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+mongoose
+  .connect(process.env.SERVER)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to db , server is running`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
