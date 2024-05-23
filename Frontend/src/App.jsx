8d3ff954504df48a8b134ec7b9f544338ab74f8c
saiddlkc <saidalkilic@gmail.com>
2024-05-23 14:34:23 +0200

    Dashboard sructure created , try to connect with Backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import Dashboard from './Components/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Dashboard></Dashboard>
     </>
   )
 }
