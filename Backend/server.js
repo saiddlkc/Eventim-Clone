be1c11405b7e639b6d7a7911281dd697904275c4
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-05-27 14:14:33 +0200

    created .env

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 import ticketsRoutes from "./router/ticketsRoutes.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+const PORT = process.env.PORT;
+const DataBase = process.env.DataBase;
 const MongoDb = async () => {
-  mongoose.connect(
-    "mongodb+srv://finalproject218:cZLcrlEDrqQGfETJ@eventim.zbmpi1k.mongodb.net/Tickets"
-  );
-  console.log("MangoDb Is Connected");
+  mongoose.connect(DataBase);
+  console.log("DataBase Is Connected");
 };
 MongoDb().catch((e) => {
   console.log(e, "error");
 });
 app.use("/tickets", ticketsRoutes);
-
-const Port = 3000;
-app.listen(Port, () => {
-  console.log("it port", Port);
+app.listen(PORT, () => {
+  console.log(`PORT ${PORT} is Connected`);
 });
