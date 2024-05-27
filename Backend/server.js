dc28199af228000873c27428bf56f256c359924d
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-05-27 09:14:13 +0200

    import ticketsRoutes in server

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import ticketsRoutes from "./router/ticketsRoutes.js";
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/tickest", ticketsRoutes);
 const MongoDb = async () => {
-  try {
-    const data = mongoose.connect(
-      "mongodb+srv://finalproject218:cZLcrlEDrqQGfETJ@eventim.zbmpi1k.mongodb.net/Tickets"
-    );
-    console.log("database connected");
-  } catch (error) {
-    console.log("database erroe", error);
-  }
+  mongoose.connect("");
+  console.log("MangoDb Is Connected");
 };
-MongoDb();
-app.get("/", (req, res) => {
-  res.json("hallo weld");
+MongoDb().catch((e) => {
+  console.log(e, "error");
 });
+
 const Port = 3000;
 app.listen(Port, () => {
   console.log("it port", Port);
