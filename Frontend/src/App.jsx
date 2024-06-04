0e47c11bfe16a3fa7948799cfc1a6ce0bb336a1c
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-06-04 13:37:19 +0200

    update app and navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,10 @@
 // import GetEvent from "./components/GetEvent";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Tickets from "./Pages/Dashboard/Tickets";
-import Add from "./Pages/Dashboard/Add";
+// import Tickets from "./Pages/Dashboard/Tickets";
+// import Add from "./Pages/Dashboard/Add";
 import Update from "./Pages/Dashboard/Update";
+import Seats from "./components/seats/Seats";
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
       {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Tickets />} />
-        {/* <Route path="/add" element={<Add />} />
-        <Route path="/update/:id" element={<Update />} /> */}
+        <Route path="/add" element={<Add />} />
+        <Route path="/seats" element={<Seats />} />
+        {/* <Route path="/update/:id" element={<Update />} />  */}
         {/* <Route path="Register" element={<Register />} />
           <Route path="Login" element={<Login />} /> */}
       </Routes>
