74f3d028e20502845462ef1e1b673f53e5cdfbaa
saiddlkc <saidalkilic@gmail.com>
2024-06-04 13:18:15 +0200

    login created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Sidebar from "./Components/Dashboard/Sidebar";
 import Topbar from "./Components/Dashboard/Topbar";
 
 import EventsTableList from "./Pages/Dashboard/EventsTableList";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route path="/users" element={<UsersTable />} />
             <Route path="/tickets" element={<TicketTable />} />
             <Route path="/events" element={<EventsTableList />} />
+            <Route path="/login" element={<Login />} /> 
           </Routes>
         </main>
       </div>
