cc8cbb4c30c3563ee8a0a496245fe16211394773
saiddlkc <saidalkilic@gmail.com>
2024-06-04 10:52:56 +0200

    styled statistics

--- Frontend/src/Pages/Dashboard/StatisticTable.jsx
+++ Frontend/src/Pages/Dashboard/StatisticTable.jsx
@@ -51,9 +51,8 @@ const Statistics = () => {
         );
         const usersCount = usersResponse.data.length + 900;
 
-        // Mock data for events, revenue, recentActivity, and revenueHistory
-        const eventsCount = 195; // Mocked number of events
-        const Umsatz = 2000; // Mocked revenue
+        const eventsCount = 195;
+        const Umsatz = 2000;
 
         const revenueHistory = [
           { date: "2023-01-01", revenue: 1000 },
@@ -118,71 +117,67 @@ const Statistics = () => {
     maintainAspectRatio: false,
   };
 
-  const activityData = [
-    { id: 1, date: "2024-06-01", action: "Ticket erstellt", user: "Admin" },
-    {
-      id: 2,
-      date: "2024-06-02",
-      action: "Veranstaltung gelöscht",
-      user: "Admin",
-    },
-    {
-      id: 3,
-      date: "2024-06-03",
-      action: "Nutzerkonto gesperrt",
-      user: "Admin",
-    },
-    { id: 4, date: "2024-06-04", action: "Ticket aktualisiert", user: "Admin" },
-    {
-      id: 5,
-      date: "2024-06-05",
-      action: "Veranstaltung erstellt",
-      user: "Admin",
-    },
-  ];
+  // const activityData = [
+  //   { id: 1, date: "2024-06-01", action: "Ticket erstellt", user: "Admin" },
+  //   {
+  //     id: 2,
+  //     date: "2024-06-02",
+  //     action: "Veranstaltung gelöscht",
+  //     user: "Admin",
+  //   },
+  //   {
+  //     id: 3,
+  //     date: "2024-06-03",
+  //     action: "Nutzerkonto gesperrt",
+  //     user: "Admin",
+  //   },
+  //   { id: 4, date: "2024-06-04", action: "Ticket aktualisiert", user: "Admin" },
+  //   {
+  //     id: 5,
+  //     date: "2024-06-05",
+  //     action: "Veranstaltung erstellt",
+  //     user: "Admin",
+  //   },
+  // ];
 
   return (
-
-
     <div className="mb-32 flex flex-col gap-12 m-2">
-  <div className="m-1 flex">
-  <div className="w-full sm:w-1/3 p-3">
-    <div className="rounded-full">
-    <Card className="h-52 rounded-full bg-blue-200 hover:bg-blue-300 transition duration-300 flex items-center justify-center">
-      <div className="rounded-full bg-white p-2">
-        <FaUser size={24} className="text-blue-500" />
+      <div className="m-1 flex justify-evenly">
+        <div className="w-48 p-3">
+          <div className="">
+            <div className="h-48  bg-blue-200 hover:bg-blue-300 transition duration-300 flex flex-col items-center justify-center border-black border-2">
+              <div className="rounded-full bg-white p-4 border-black border-2">
+                <FaUser size={24} className="text-blue-500" />
+              </div>
+              <Typography variant="h2" color="black">
+                {stats.users}
+              </Typography>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-48 mt-4 sm:mt-0 p-3">
+          <div className="h-48  bg-gray-500 hover:bg-gray-700 transition duration-300 flex flex-col  items-center justify-center border-black border-2">
+            <div className="rounded-full bg-white p-4 border-black border-2">
+              <FaCalendarAlt size={24} className="text-black" />
+            </div>
+            <Typography variant="h2" color="black">
+              {stats.events}
+            </Typography>
+          </div>
+        </div>
+
+        <div className="w-48 mt-4 sm:mt-0 p-3">
+          <div className="h-48  bg-green-200 hover:bg-green-300 transition duration-300 flex  flex-col  items-center justify-center border-black border-2">
+            <div className="rounded-full bg-white p-4 border-black border-2">
+              <FaMoneyBillAlt size={24} className="text-green-500 " />
+            </div>
+            <Typography variant="h2" color="black">
+              {stats.revenue + "€"}
+            </Typography>
+          </div>
+        </div>
       </div>
-      <Typography variant="h2" color="black">
-        {stats.users}
-      </Typography>
-    </Card>
-    </div>
-  </div>
-
-  <div className="w-full sm:w-1/3 mt-4 sm:mt-0 p-3">
-    <Card className="h-52 rounded-full bg-gray-700 hover:bg-gray-300 transition duration-300 flex items-center justify-center">
-      <div className="rounded-full bg-white p-2">
-        <FaCalendarAlt size={24} className="text-black" />
-      </div>
-      <Typography variant="h2" color="black" >
-        {stats.events}
-      </Typography>
-    </Card>
-  </div>
-
-  <div className="w-full sm:w-1/3 mt-4 sm:mt-0 p-3">
-    <Card className="h-52 rounded-full bg-green-200 hover:bg-green-300 transition duration-300 flex items-center justify-center">
-      <div className="rounded-full bg-white p-2">
-        <FaMoneyBillAlt size={24} className="text-green-500" />
-      </div>
-      <Typography variant="h2" color="black" >
-        {stats.revenue + "€"}
-      </Typography>
-    </Card>
-  </div>
-</div>
-
-
 
       <div className="m-1">
         <div className="h-64 w-full mb-4">
