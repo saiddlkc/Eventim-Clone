9016c2e79ba59ca12c6266444cebb36028696bef
saiddlkc <saidalkilic@gmail.com>
2024-06-04 10:23:49 +0200

    charts styled

--- Frontend/src/Pages/Dashboard/StatisticTable.jsx
+++ Frontend/src/Pages/Dashboard/StatisticTable.jsx
@@ -13,8 +13,14 @@ import {
   LineElement,
   PointElement,
 } from "chart.js";
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import { Card, CardHeader, CardBody, Typography } from "@material-tailwind/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Table,
+} from "@material-tailwind/react";
+import { FaUser, FaCalendarAlt, FaMoneyBillAlt } from "react-icons/fa";
 
 ChartJS.register(
   CategoryScale,
@@ -25,8 +31,7 @@ ChartJS.register(
   Legend,
   ArcElement,
   LineElement,
-  PointElement,
-  ChartDataLabels
+  PointElement
 );
 
 const Statistics = () => {
@@ -101,104 +106,113 @@ const Statistics = () => {
     ],
   };
 
-  const middleBarData = {
-    labels: ["Users", "Tickets", "Revenues"],
-    datasets: [
-      {
-        label: "Statistics",
-        data: [stats.users, stats.events, stats.revenue],
-        backgroundColor: ["#040414", "#000086", "#10B981"],
-        borderColor: ["#040414", "#000086", "#10B981"],
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const optionsWithLabels = {
+  const options = {
     plugins: {
-      datalabels: {
+      legend: {
         display: true,
-        color: 'white',
-        anchor: 'end',
-        align: 'start',
-        formatter: (value) => value,
+      },
+      tooltip: {
+        enabled: true,
       },
     },
     maintainAspectRatio: false,
   };
 
+  const activityData = [
+    { id: 1, date: "2024-06-01", action: "Ticket erstellt", user: "Admin" },
+    {
+      id: 2,
+      date: "2024-06-02",
+      action: "Veranstaltung gelöscht",
+      user: "Admin",
+    },
+    {
+      id: 3,
+      date: "2024-06-03",
+      action: "Nutzerkonto gesperrt",
+      user: "Admin",
+    },
+    { id: 4, date: "2024-06-04", action: "Ticket aktualisiert", user: "Admin" },
+    {
+      id: 5,
+      date: "2024-06-05",
+      action: "Veranstaltung erstellt",
+      user: "Admin",
+    },
+  ];
+
   return (
+
+
     <div className="mb-32 flex flex-col gap-12 m-2">
+  <div className="m-1 flex">
+  <div className="w-full sm:w-1/3 p-3">
+    <div className="rounded-full">
+    <Card className="h-52 rounded-full bg-blue-200 hover:bg-blue-300 transition duration-300 flex items-center justify-center">
+      <div className="rounded-full bg-white p-2">
+        <FaUser size={24} className="text-blue-500" />
+      </div>
+      <Typography variant="h2" color="black">
+        {stats.users}
+      </Typography>
+    </Card>
+    </div>
+  </div>
+
+  <div className="w-full sm:w-1/3 mt-4 sm:mt-0 p-3">
+    <Card className="h-52 rounded-full bg-gray-700 hover:bg-gray-300 transition duration-300 flex items-center justify-center">
+      <div className="rounded-full bg-white p-2">
+        <FaCalendarAlt size={24} className="text-black" />
+      </div>
+      <Typography variant="h2" color="black" >
+        {stats.events}
+      </Typography>
+    </Card>
+  </div>
+
+  <div className="w-full sm:w-1/3 mt-4 sm:mt-0 p-3">
+    <Card className="h-52 rounded-full bg-green-200 hover:bg-green-300 transition duration-300 flex items-center justify-center">
+      <div className="rounded-full bg-white p-2">
+        <FaMoneyBillAlt size={24} className="text-green-500" />
+      </div>
+      <Typography variant="h2" color="black" >
+        {stats.revenue + "€"}
+      </Typography>
+    </Card>
+  </div>
+</div>
+
+
+
       <div className="m-1">
-        <Card className="h-64 w-full mb-6">
-          <CardHeader floated={false} className="rounded-none">
-            <div className="mb-8 flex items-center justify-between gap-8">
-              <div>
-                <Typography variant="h2">
-                  Live Statistics
-                </Typography>
-              </div>
-            </div>
-          </CardHeader>
-          <CardBody className="px-0">
+        <div className="h-64 w-full mb-4">
+          <CardBody className="px-0 ">
             <div className="p-4 rounded overflow-auto h-96">
-              <Typography variant="h6" color="black" className="mb-4">
+              <Typography variant="h6" color="black" className="mb-1">
                 Statistics Overview
               </Typography>
               <div className="h-72">
-                <Bar data={barData} options={{ maintainAspectRatio: false }} />
+                <Bar data={barData} options={options} />
               </div>
             </div>
           </CardBody>
-        </Card>
+        </div>
       </div>
 
       <div className="m-1">
-        <Card className="h-64 w-full mb-6 mt-24">
-          <CardHeader floated={false} className="rounded-none">
-            <div className="mb-8 flex items-center justify-between gap-8">
-              <div>
-                <Typography variant="h2">
-                  Revenue
-                </Typography>
-              </div>
-            </div>
-          </CardHeader>
-          <CardBody className="px-0">
+        <div className="h-64 w-full mb-4 mt-24">
+          <CardHeader floated={false} className="rounded-none"></CardHeader>
+          <CardBody className="px-0 mt-2 ">
             <div className="rounded p-4 overflow-auto h-96">
-              <Typography variant="h6" color="black" className="mb-4">
+              <Typography variant="h6" color="black" className="mb-1">
                 Statistics Distribution
               </Typography>
               <div className="h-72">
-                <Line data={lineData} options={{ maintainAspectRatio: false }} />
+                <Line data={lineData} options={options} />
               </div>
             </div>
           </CardBody>
-        </Card>
-      </div>
-
-      <div className="m-1">
-        <Card className="h-64 w-full mb-6 mt-24">
-          <CardHeader floated={false} className="rounded-none">
-            <div className="mb-8 flex items-center justify-between gap-8">
-              <div>
-                <Typography variant="h2">
-                  Users, Tickets and Revenues
-                </Typography>
-              </div>
-            </div>
-          </CardHeader>
-          <CardBody className="px-0">
-            
-              <Typography variant="h6" color="black" className="mb-4">
-                Numbers Overview
-              </Typography>
-              <div className="h-72">
-                <Bar data={middleBarData} options={optionsWithLabels} />
-              </div>
-           
-          </CardBody>
-        </Card>
+        </div>
       </div>
     </div>
   );
