d90c5f910885b80b48250b717903a2d1f6dc41eb
DrCetmi <drcetmi@gmail.com>
2024-06-04 13:12:42 +0200

    adminlayout added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,24 @@
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import UsersTable from "./Components/Dashboard/Table";
 import TicketTable from "./Pages/Dashboard/TicketTable";
 import Statistics from "./Pages/Dashboard/StatisticTable";
-import Sidebar from "./Components/Dashboard/Sidebar";
-import Topbar from "./Components/Dashboard/Topbar";
 import Profile from "./Pages/Dashboard/Profile";
-
 import EventsTableList from "./Pages/Dashboard/EventsTableList";
+import SignUp from "./Pages/Login/Register";
+import AdminLayout from "./Layout/AdminLayout";
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-      <div
-        className="flex-1 flex flex-col m-4 p-6"
-        style={{ marginLeft: "250px" }}
-      >
-        <Topbar />
-        <main className="flex-1 p-2">
-          <Routes>
-            <Route index element={<Statistics />} />
-            <Route path="/users" element={<UsersTable />} />
-            <Route path="/tickets" element={<TicketTable />} />
-            <Route path="/events" element={<EventsTableList />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </main>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route element={<AdminLayout />}>
+        <Route index element={<Statistics />} />
+        <Route path="/users" element={<UsersTable />} />
+        <Route path="/tickets" element={<TicketTable />} />
+        <Route path="/events" element={<EventsTableList />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Routes>
   );
 }
 
