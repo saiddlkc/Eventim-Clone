38fd7bd89d3aecd1c6f749133ed606bf33d9e3df
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-06-05 02:15:13 +0200

    change add and tickets

--- Frontend/src/Pages/Dashboard/Add.jsx
+++ Frontend/src/Pages/Dashboard/Add.jsx
@@ -84,7 +84,7 @@ export function Tickets() {
     };
     fetchTickets();
   }, []);
-
+  
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   };
