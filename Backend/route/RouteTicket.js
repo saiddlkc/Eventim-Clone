2cd9088bec923cd8477953bf6a054d17951f2ef1
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-06-06 14:17:01 +0200

    create route

--- Backend/route/RouteTicket.js
+++ Backend/route/RouteTicket.js
@@ -2,8 +2,20 @@ import Ticktes from "../model/Tickets.js";
 import express from "express";
 
 const route = express.Router();
-route.get("/", async (req, res) => {
-  try {
-    const tickets = await Ticktes.create(r);
-  } catch (error) {}
-});
+route
+  .get("/", async (req, res) => {
+    try {
+      const tickets = await Ticktes.find();
+      res.status(200).json(tickets);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  })
+  .post("/", async (req, res) => {
+    try {
+      const tickets = await Ticktes.create({});
+      res.status(200).json(tickets);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
