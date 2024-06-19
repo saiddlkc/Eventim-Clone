12d61db8052687aecfb96ccf324b056e9bf2ae45
daniel behbudi <mbehbudi@gmail.com>
2024-06-19 11:26:34 +0200

    event update

--- Frontend/src/Pages/Dashboard/EventsTableList.jsx
+++ Frontend/src/Pages/Dashboard/EventsTableList.jsx
@@ -345,52 +345,57 @@ const EventsTableList = () => {
               />
             </>
           ) : (
-            <Typography
-              variant="small"
-              color="blue-gray"
-              className="font-normal"
-            >
-              {original.veranstalter.name},{" "}
-              {original.veranstalter.kontakt.email}
-            </Typography>
+            <>
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="font-normal"
+              >
+                {original.veranstalter.name}
+              </Typography>
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="font-normal"
+              >
+                {original.veranstalter.kontakt.email}
+              </Typography>
+            </>
           ),
       },
       {
         Header: "Aktionen",
+        accessor: "_id",
         Cell: ({ row: { original } }) =>
           editEventId === original._id ? (
             <>
-              <Tooltip content="Save">
-                <IconButton variant="text" color="green" onClick={handleSave}>
-                  <FaSave className="h-4 w-4" />
-                </IconButton>
-              </Tooltip>
-              <Tooltip content="Cancel">
-                <IconButton variant="text" color="red" onClick={handleCancel}>
-                  <FaTimes className="h-4 w-4" />
-                </IconButton>
-              </Tooltip>
+              <Button
+                onClick={handleSave}
+                className="bg-blue-500 text-white px-2 py-1 rounded-md"
+              >
+                <FaSave />
+              </Button>
+              <Button
+                onClick={handleCancel}
+                className="bg-red-500 text-white px-2 py-1 rounded-md"
+              >
+                <FaTimes />
+              </Button>
             </>
           ) : (
             <>
-              <Tooltip content="Edit">
-                <IconButton
-                  variant="text"
-                  color="blue"
-                  onClick={() => handleEdit(original)}
-                >
-                  <FaEdit className="h-4 w-4" />
-                </IconButton>
-              </Tooltip>
-              <Tooltip content="Delete">
-                <IconButton
-                  variant="text"
-                  color="red"
-                  onClick={() => handleDelete(original._id)}
-                >
-                  <FaTrashAlt className="h-4 w-4" />
-                </IconButton>
-              </Tooltip>
+              <Button
+                onClick={() => handleEdit(original)}
+                className="bg-green-500 text-white px-2 py-1 rounded-md"
+              >
+                <FaEdit />
+              </Button>
+              <Button
+                onClick={() => handleDelete(original._id)}
+                className="bg-red-500 text-white px-2 py-1 rounded-md"
+              >
+                <FaTrashAlt />
+              </Button>
             </>
           ),
       },
@@ -398,12 +403,14 @@ const EventsTableList = () => {
     [editEventId, editEventData]
   );
 
+  const data = useMemo(() => events, [events]);
+
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    page,
     prepareRow,
+    page,
     canPreviousPage,
     canNextPage,
     pageOptions,
@@ -416,251 +423,224 @@ const EventsTableList = () => {
   } = useTable(
     {
       columns,
-      data: events,
-      initialState: { pageIndex: 0, pageSize: 5 },
+      data,
     },
     useSortBy,
     usePagination
   );
 
   return (
-    <div className="mt-12 mb-8 flex flex-col gap-12">
-      <ToastContainer />
-      <div>
-        <Card>
-          <CardHeader variant="gradient" color="blue" className="mb-8 p-6">
-            <div className="flex justify-between">
-              <Typography variant="h6" color="white">
-                Add New Event
-              </Typography>
-              <Button
-                color="blue"
-                size="small"
-                onClick={() => setShowNewEventForm(!showNewEventForm)}
-              >
-                {showNewEventForm ? "Close Form" : "New Event"}
-              </Button>
-            </div>
-          </CardHeader>
-          {showNewEventForm && (
-            <CardBody className="pt-0">
-              <div className="flex flex-col gap-4">
-                <Input
-                  label="Title"
-                  name="titel"
-                  value={newEvent.titel}
-                  onChange={handleNewEventChange}
-                />
-                <Input
-                  label="Category"
-                  name="kategorie"
-                  value={newEvent.kategorie}
-                  onChange={handleNewEventChange}
-                />
-                <Input
-                  label="Description"
-                  name="beschreibung"
-                  value={newEvent.beschreibung}
-                  onChange={handleNewEventChange}
-                />
-                <div className="flex gap-4">
-                  <Input
-                    type="date"
-                    label="Start Date"
-                    name="startDatum"
-                    value={newEvent.startDatum}
-                    onChange={handleNewEventChange}
-                  />
-                  <Input
-                    type="date"
-                    label="End Date"
-                    name="endDatum"
-                    value={newEvent.endDatum}
-                    onChange={handleNewEventChange}
-                  />
-                </div>
-                <Input
-                  label="Image URL"
-                  name="bild"
-                  value={newEvent.bild}
-                  onChange={handleNewEventChange}
-                />
-                <div className="flex gap-4">
-                  <Input
-                    label="Address"
-                    name="adresse"
-                    value={newEvent.ort.adresse}
-                    onChange={handleNewEventChange}
-                  />
-                  <Input
-                    label="City"
-                    name="stadt"
-                    value={newEvent.ort.stadt}
-                    onChange={handleNewEventChange}
-                  />
-                </div>
-                <div className="flex gap-4">
-                  <Input
-                    label="State"
-                    name="bundesland"
-                    value={newEvent.ort.bundesland}
-                    onChange={handleNewEventChange}
-                  />
-                  <Input
-                    label="Country"
-                    name="land"
-                    value={newEvent.ort.land}
-                    onChange={handleNewEventChange}
-                  />
-                </div>
-                <Input
-                  label="Organizer Name"
-                  name="veranstalterName"
-                  value={newEvent.veranstalter.name}
-                  onChange={handleNewEventChange}
-                />
-                <Input
-                  label="Contact Email"
-                  name="kontaktEmail"
-                  type="email"
-                  value={newEvent.veranstalter.kontakt.email}
-                  onChange={handleNewEventChange}
-                />
-                <Button color="green" onClick={handleAddEvent}>
-                  Add Event
-                </Button>
-              </div>
-            </CardBody>
-          )}
-        </Card>
-      </div>
-
-      <div className="mt-6">
-        <Card className="h-full w-full bg-blue-gray-100">
-          <CardHeader
-            floated={false}
-            shadow={false}
-            className="rounded-none bg-blue-gray-100"
-          >
-            <div className="flex items-center justify-between gap-8">
-              <Typography variant="h5" color="blue-gray">
-                Events
-              </Typography>
-              <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
-                <div className="w-full md:w-72">
-                  <Input
-                    label="Search"
-                    icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-                  />
-                </div>
-              </div>
-            </div>
-          </CardHeader>
-          <CardBody className="overflow-scroll px-0">
-            <table
-              className="mt-4 w-full min-w-max table-auto text-left"
-              {...getTableProps()}
+    <Card>
+      <CardHeader className="flex justify-between">
+        <Typography variant="h5">Events</Typography>
+        <Button onClick={() => setShowNewEventForm(!showNewEventForm)}>
+          <UserPlusIcon className="h-5 w-5 text-white" />
+          Add New Event
+        </Button>
+      </CardHeader>
+      <CardBody>
+        {showNewEventForm && (
+          <div className="space-y-4 mb-4">
+            <input
+              type="text"
+              name="titel"
+              placeholder="Titel"
+              value={newEvent.titel}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="kategorie"
+              placeholder="Kategorie"
+              value={newEvent.kategorie}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="beschreibung"
+              placeholder="Beschreibung"
+              value={newEvent.beschreibung}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="date"
+              name="startDatum"
+              value={newEvent.startDatum}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="date"
+              name="endDatum"
+              value={newEvent.endDatum}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="bild"
+              placeholder="Bild URL"
+              value={newEvent.bild}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="adresse"
+              placeholder="Adresse"
+              value={newEvent.ort.adresse}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="stadt"
+              placeholder="Stadt"
+              value={newEvent.ort.stadt}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="bundesland"
+              placeholder="Bundesland"
+              value={newEvent.ort.bundesland}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="land"
+              placeholder="Land"
+              value={newEvent.ort.land}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="text"
+              name="veranstalterName"
+              placeholder="Veranstalter Name"
+              value={newEvent.veranstalter.name}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <input
+              type="email"
+              name="kontaktEmail"
+              placeholder="Veranstalter Email"
+              value={newEvent.veranstalter.kontakt.email}
+              onChange={handleNewEventChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+            />
+            <Button
+              onClick={handleAddEvent}
+              className="bg-blue-500 text-white px-4 py-2 rounded"
             >
-              <thead>
-                {headerGroups.map((headerGroup) => (
-                  <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
-                      <th
-                        {...column.getHeaderProps(
-                          column.getSortByToggleProps()
-                        )}
-                        className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
-                      >
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
-                        >
-                          {column.render("Header")}
-                          <ChevronUpDownIcon
-                            className="h-4 w-4"
-                            strokeWidth={2}
-                          />
-                        </Typography>
-                      </th>
-                    ))}
-                  </tr>
+              Add Event
+            </Button>
+          </div>
+        )}
+        <table
+          {...getTableProps()}
+          className="min-w-full divide-y divide-gray-200"
+        >
+          <thead className="bg-gray-50">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    {column.render("Header")}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? " ▼"
+                          : " ▲"
+                        : ""}
+                    </span>
+                  </th>
                 ))}
-              </thead>
-              <tbody {...getTableBodyProps()}>
-                {page.map((row) => {
-                  prepareRow(row);
-                  return (
-                    <tr {...row.getRowProps()}>
-                      {row.cells.map((cell) => (
-                        <td
-                          className="p-4 border-b border-blue-gray-50"
-                          {...cell.getCellProps()}
-                        >
-                          {cell.render("Cell")}
-                        </td>
-                      ))}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </CardBody>
-          <CardFooter>
-            <div className="flex justify-between">
-              <Button
-                variant="text"
-                onClick={() => previousPage()}
-                disabled={!canPreviousPage}
-              >
-                Previous
-              </Button>
-              <div className="flex items-center gap-2">
-                <span>
-                  Page{" "}
-                  <strong>
-                    {pageIndex + 1} of {pageOptions.length}
-                  </strong>{" "}
-                </span>
-                <span>
-                  | Go to page:{" "}
-                  <input
-                    type="number"
-                    defaultValue={pageIndex + 1}
-                    onChange={(e) => {
-                      const page = e.target.value
-                        ? Number(e.target.value) - 1
-                        : 0;
-                      gotoPage(page);
-                    }}
-                    style={{ width: "50px" }}
-                  />
-                </span>
-                <select
-                  value={pageSize}
-                  onChange={(e) => {
-                    setPageSize(Number(e.target.value));
-                  }}
-                >
-                  {[5, 10, 20, 30, 40, 50].map((pageSize) => (
-                    <option key={pageSize} value={pageSize}>
-                      Show {pageSize}
-                    </option>
+              </tr>
+            ))}
+          </thead>
+          <tbody
+            {...getTableBodyProps()}
+            className="bg-white divide-y divide-gray-200"
+          >
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td
+                      {...cell.getCellProps()}
+                      className="px-6 py-4 whitespace-nowrap"
+                    >
+                      {cell.render("Cell")}
+                    </td>
                   ))}
-                </select>
-              </div>
-              <Button
-                variant="text"
-                onClick={() => nextPage()}
-                disabled={!canNextPage}
-              >
-                Next
-              </Button>
-            </div>
-          </CardFooter>
-        </Card>
-      </div>
-    </div>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div className="pagination">
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+            {"<<"}
+          </button>{" "}
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            {"<"}
+          </button>{" "}
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            {">"}
+          </button>{" "}
+          <button
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {">>"}
+          </button>{" "}
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <span>
+            | Go to page:{" "}
+            <input
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: "100px" }}
+            />
+          </span>{" "}
+          <select
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+      </CardBody>
+      <CardFooter>
+        <ToastContainer />
+      </CardFooter>
+    </Card>
   );
 };
 
