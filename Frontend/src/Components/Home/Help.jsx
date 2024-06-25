fc4835be2b73695b95038382c0199e05690cfa8f
saiddlkc <saidalkilic@gmail.com>
2024-06-25 11:00:20 +0200

    help component

--- Frontend/src/Components/Home/Help.jsx
+++ Frontend/src/Components/Home/Help.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Help = () => {
   return (
     <div className="container mx-auto w-full ">
-      <Link to="/Contact-us">
+      <Link to="/contact">
         <img
           className="w-full"
           src="https://www.eventim.de/obj/media/DE-eventim/teaser/de/additional-content/2023/eve-help-center-additional-content-d-03.jpg"
